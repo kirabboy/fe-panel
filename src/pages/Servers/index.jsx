34384447af44b15cc72a07fa5bfe95d8374b13f1
hautv140802 <hautv1408@gmail.com>
2024-09-22 22:56:03 +0700

    feat: create ui button (#4)

--- src/pages/Servers/index.jsx
+++ src/pages/Servers/index.jsx
@@ -1,9 +1,16 @@
+import ButtonComponent from '../../components/Button';
 import CardWrapper from '../../components/CardWrapper/CardWrapper';
 
 const Servers = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-[1.2rem]">
       <CardWrapper title="Server title">Server content</CardWrapper>
+
+      <div className="flex gap-[1.2rem]">
+        <ButtonComponent href="#" text="Preview" />
+        <ButtonComponent type={'text'} text="Preview" />
+        <ButtonComponent text="Buy now" />
+      </div>
     </div>
   );
 };
